f41950c744a71bf9778c783bc09f3b07c283c216
TaishiNishimura <taishi19880324@gmail.com>
2021-07-11 15:56:23 +0000

    7/12/0:56

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     book.save
     redirect_to books_path
   end
